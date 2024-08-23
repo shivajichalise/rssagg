ff44428088443ef47c022c0ff2eea7334369ee66
Shivaji Chalise <respond2shivaji@gmail.com>
2024-08-23 08:32:18 +0545

    http server boilerplate

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("ERROR: Error loading .env file")
+	}
+
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		log.Fatal("ERROR: Port is not defined")
+	}
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerError)
+
+	router.Mount("/v1", v1Router)
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + PORT,
+	}
+
+	fmt.Println("INFO: Server is starting on port: ", PORT)
+	server_err := server.ListenAndServe()
+	if server_err != nil {
+		log.Fatal(err)
+	}
 }
